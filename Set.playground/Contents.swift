dedd83ddcbc52e9cd7e26eb4f0dd26bf3789c886
EunJi01 <hej0102@naver.com>
2022-06-05 19:56:24 +0900

    fix: Set

--- Set.playground/Contents.swift
+++ Set.playground/Contents.swift
@@ -11,7 +11,8 @@ print(result)
 print(two.union(three)) // [9, 4, 10, 15, 6, 8, 2, 12, 14, 3, 18]
 
 // 차집합
-print(two.subtract(three)) // ()
+print(two.subtracting(three)) // [14, 2, 4, 8, 10]
+// 강의에서는 subtract로 나와있으나, subtract를 사용하면 ()로 출력되어 subtractin을 사용하니 의도한 값이 나왔다!
 
-// 여집합
+// 대칭자 -> 합집합에서 교집합을 뺀 것
 print(two.symmetricDifference(three)) // [9, 18, 12, 6, 14, 15, 8, 2, 4, 10, 3]
